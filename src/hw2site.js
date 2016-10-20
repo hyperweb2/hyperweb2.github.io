8920c1ef3153e466cd2f77ea1e13f94cae164f85
Yehonal <yehonal.azeroth@gmail.com>
2016-10-20 13:53:36 +0200

    some fixes

--- src/hw2site.js
+++ src/hw2site.js
@@ -1,9 +1,15 @@
-var app=angular.module("Hw2Site",["ngRoute"]);
+var app=angular.module("Hw2Site",["ngRoute","ngAnimate","angular-loading-bar"]);
 
-app.config(['$routeProvider','$compileProvider',function($routeProvider,$compileProvider){
+app.config(['$routeProvider','$locationProvider','cfpLoadingBarProvider',function($routeProvider,$locationProvider,cfpLoadingBarProvider){
+    cfpLoadingBarProvider.includeSpinner = true;
+ 
     $routeProvider
-    .when("/",                {templateUrl: "src/pages/home.html"})
+    .when("/index.html",      {templateUrl: "src/pages/home.html", reloadOnSearch: false})
     .when("/media.html",      {templateUrl: "src/pages/media.html"})
     .when("/partners.html",   {templateUrl: "src/pages/partners.html"})
+    .when("/prodotti.html",   {templateUrl: "src/pages/prodotti.html"})
     .when("/terms.html",      {templateUrl: "src/pages/terms.html"})
+    .otherwise( {redirectTo: '/index.html'});
+    
+    $locationProvider.html5Mode(true);
 }]);
