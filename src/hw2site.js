db806a536a5a49507051dda8075db8026e5bc887
Yehonal <yehonal.azeroth@gmail.com>
2016-10-21 20:21:27 +0200

    updates

--- src/hw2site.js
+++ src/hw2site.js
@@ -1,28 +1,28 @@
 if (window["angular"] == null) {
-    var url = window.location.protocol + '//' + window.location.host + '/#' + window.location.pathname;
+    var url = window.location.protocol + '//' + window.location.host + '/#' + window.location.pathname + window.location.hash;
     window.location.replace(url);
 }
 
 var app=angular.module("Hw2Site",["ngRoute",'angular-loading-bar']);
 
-app.config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider){   
+app.config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider){              
     $routeProvider
     .when("/site/it/home.html",       {templateUrl: "site/it/home.html", reloadOnSearch: false})
     .when("/site/it/media.html",      {templateUrl: "site/it/media.html", reloadOnSearch: false})
     .when("/site/it/partners.html",   {templateUrl: "site/it/partners.html", reloadOnSearch: false})
     .when("/site/it/prodotti.html",   {templateUrl: "site/it/prodotti.html", reloadOnSearch: false})
-    .when("/terms/",          {redirectTo: 'site/en/terms.html'})
     .when("/site/en/terms.html",      {templateUrl: "site/en/terms.html", reloadOnSearch: false})
-    .otherwise( {redirectTo: '/site/it/home.html'});
-    
+    .when("/terms/",          {redirectTo: 'site/en/terms.html'})
+    .otherwise(               {redirectTo: '/site/it/home.html'});
+        
     $locationProvider.html5Mode(true);
 }]);
 
 app.run(function($rootScope) {
-    $rootScope.$on("routeChangeStart", function($event,$currentRoute, $previousRoute) {
-        if ($previousRoute != null && $currentRoute.loadedTemplateUrl != $previousRoute.loadedTemplateUrl 
-                && $currentRoute.loadedTemplateUrl == "site/it/home.html") {
-            console.log("to reload");
+    $rootScope.$on("$routeChangeStart", function($event,$next, $previousRoute) {
+        if ($previousRoute != null && $next.$$route.templateUrl != $previousRoute.loadedTemplateUrl 
+                && $next.$$route.templateUrl == "site/it/home.html") {
+            window.location.reload(true);
         }
     });
 });
