4bc0ea2e8f2685a247c7a58130800f44f3e24754
Yehonal <yehonal.azeroth@gmail.com>
2016-10-21 08:52:16 +0200

    update

--- assets/theme/js/script.js
+++ assets/theme/js/script.js
@@ -465,14 +465,4 @@
 
 })(jQuery);
 !function() {
-    try {
-        document.getElementsByClassName('engine')[0].getElementsByTagName('a')[0].removeAttribute('rel');
-    } catch(err){ }
-    if(!document.getElementById('top-1')) {
-        var e = document.createElement("section");
-        e.id = "top-1";
-        e.className = "engine";
-        e.innerHTML = '<a href="https://mobirise.com">mobirise.com</a> Mobirise v3.8.1';
-        document.body.insertBefore(e, document.body.childNodes[0]);
-    }
 }();
--- src/hw2site.js
+++ src/hw2site.js
@@ -1,15 +1,17 @@
 var app=angular.module("Hw2Site",["ngRoute","ngAnimate","angular-loading-bar"]);
 
-app.config(['$routeProvider','$locationProvider','cfpLoadingBarProvider',function($routeProvider,$locationProvider,cfpLoadingBarProvider){
+app.config(['$routeProvider','$locationProvider','cfpLoadingBarProvider','$anchorScrollProvider',function($routeProvider,$locationProvider,cfpLoadingBarProvider,$anchorScrollProvider){
     cfpLoadingBarProvider.includeSpinner = true;
+    $anchorScrollProvider.disableAutoScrolling();
  
     $routeProvider
-    .when("/home.html",      {templateUrl: "src/pages/home.html", reloadOnSearch: false})
-    .when("/media.html",      {templateUrl: "src/pages/media.html"})
-    .when("/partners.html",   {templateUrl: "src/pages/partners.html"})
-    .when("/prodotti.html",   {templateUrl: "src/pages/prodotti.html"})
-    .when("/terms.html",      {templateUrl: "src/pages/terms.html"})
+    .when("/home.html",       {templateUrl: "src/pages/home.html", reloadOnSearch: false})
+    .when("/media.html",      {templateUrl: "src/pages/media.html", reloadOnSearch: false})
+    .when("/partners.html",   {templateUrl: "src/pages/partners.html", reloadOnSearch: false})
+    .when("/prodotti.html",   {templateUrl: "src/pages/prodotti.html", reloadOnSearch: false})
+    .when("/terms.html",      {templateUrl: "src/pages/terms.html", reloadOnSearch: false})
     .otherwise( {redirectTo: '/home.html'});
     
     $locationProvider.html5Mode(true);
 }]);
+
