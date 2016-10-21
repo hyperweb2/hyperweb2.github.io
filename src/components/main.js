4dc5ebb70dd0c44435578808b74a12a89b66bb86
Yehonal <yehonal.azeroth@gmail.com>
2016-10-22 00:34:50 +0200

    update

--- src/components/main.js
+++ src/components/main.js
@@ -1,2 +1,4 @@
-$.get("src/components/main-header.html", function(data) { $('body').append(data); });
-$.get("src/components/main-footer.html", function(data) { $('body').append(data); });
+$(document).ready(function() {
+    $.get("src/components/main-header.html", function(data) { $('body').append(data); });
+    $.get("src/components/main-footer.html", function(data) { $('body').append(data); });
+});
