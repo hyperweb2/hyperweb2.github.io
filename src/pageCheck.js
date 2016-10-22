2a0f199228ab620eabb0ee752c7a2029a56319c9
Yehonal <yehonal.azeroth@gmail.com>
2016-10-22 20:17:03 +0200

    restored old method

--- src/pageCheck.js
+++ src/pageCheck.js
@@ -1,28 +1,5 @@
-if (!window["angular"]) {
-    function httpGet(theUrl,callback)
-    {
-        if (window.XMLHttpRequest)
-        {// code for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttp=new XMLHttpRequest();
-        }
-        else
-        {// code for IE6, IE5
-            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onreadystatechange=function()
-        {
-            if (xmlhttp.readyState==4 && xmlhttp.status==200)
-            {
-                callback(xmlhttp.responseText);
-            }
-        }
-        xmlhttp.open("GET", theUrl, false );
-        xmlhttp.send();    
-    }    
-    
-    httpGet("/index.html",function(data) { 
-        var newDoc = document.open("text/html", "replace");
-        newDoc.write(data);
-        newDoc.close();
-    });
+if (window["angular"] == null) {
+    var url = window.location.protocol + '//' + window.location.host + '/#' + window.location.pathname + window.location.hash;
+    window.location.replace(url);
+    throw new Error("Abort!");
 }
